3ccd88cb9f2d98d804a87962ea6d06b4ab95c277
skwita <skwita.max@gmail.com>
2022-11-22 14:13:05 +0300

    added text input

--- src/main/java/com/skwita/turtle/controller/MainController.java
+++ src/main/java/com/skwita/turtle/controller/MainController.java
@@ -1,21 +1,13 @@
 package com.skwita.turtle.controller;
 
-import com.skwita.turtle.model.Turtle;
-import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.awt.image.BufferedImage;
-
 @RestController
 public class MainController {
-    private final Turtle turtle;
-    @Autowired
-    public MainController(Turtle turtle) {
-        this.turtle = turtle;
-    }
+    @GetMapping("/")
+    public void main(@RequestBody String input) {
 
-    public BufferedImage main(@RequestBody String input) {
-        return turtle.process(input);
     }
 }
