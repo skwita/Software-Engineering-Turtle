7d4719ed0f049a24085a1bafd1d5ed9f203cea45
skwita <skwita.max@gmail.com>
2022-11-22 17:50:17 +0300

    implemented the ability of movement

--- src/main/java/com/skwita/turtle/controller/MainController.java
+++ src/main/java/com/skwita/turtle/controller/MainController.java
@@ -1,13 +1,21 @@
 package com.skwita.turtle.controller;
 
+import com.skwita.turtle.model.Turtle;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class MainController {
+    private final Turtle turtle;
+    @Autowired
+    public MainController(Turtle turtle) {
+        this.turtle = turtle;
+    }
+
     @GetMapping("/")
     public void main(@RequestBody String input) {
-
+        turtle.process(input);
     }
 }
--- src/main/java/com/skwita/turtle/model/Turtle.java
+++ src/main/java/com/skwita/turtle/model/Turtle.java
@@ -3,10 +3,29 @@ package com.skwita.turtle.model;
 import org.springframework.stereotype.Component;
 
 import java.awt.image.BufferedImage;
+import java.util.Arrays;
 
 @Component
 public class Turtle {
+    private char[][] field = new char[64][64];
     public BufferedImage process(String input) {
+        fillArray(input);
         return new BufferedImage(640, 640, BufferedImage.TYPE_INT_RGB);
+    }
+
+    private void fillArray(String input) {
+        for (char[] row: field) {
+            Arrays.fill(row, '-');
+        }
+        int curX = 0;
+        int curY = 0;
+        for (char aChar: input.toCharArray()) {
+            switch (aChar) {
+                case '>': curX += curX < 64 ? 1 : 0; break;
+                case '<': curX -= curX > 0 ? 1 : 0; break;
+                case 'V': curY += curY < 64 ? 1 : 0; break;
+                case '^': curY -= curY > 0 ? 1 : 0; break;
+            }
+        }
     }
 }
